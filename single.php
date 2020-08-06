5b0ba4aa5142aaf63b23078a5fe1a9e5732f2c4c
tosshii <sj.syj.bumo@gmail.com>
2020-08-06 16:15:05 +0900

    single.phpにファーストビューを追加し、記事タイトルの表示位置を変更。

--- single.php
+++ single.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
 
+  <!-- ファーストビュー -->
+  <div class="fv">
+    <div class="fv-title-wrapper">
+      <h2 class="site-title">Toshiki Portfolio</h2><!-- /.site-title -->
+      <p class="sub-title">- Product -</p><!-- /.sub-title -->
+    </div><!-- /.fv-title-wrapper -->
+  </div><!-- /.fv -->
+
   <main>
     <!-- メインコンテンツ -->
     <section class="section">
@@ -8,6 +16,7 @@
         <?php while(have_posts()) : ?>
           <?php the_post(); ?>
           <div class="section-inner">
+            <h2 class="section-title"><?php the_title(); ?></h2><!-- /.section-title -->
             <?php the_content(); ?>
           </div><!-- /.section-inner -->
         <?php endwhile; ?>
