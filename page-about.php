ea0dcb45ddb8ab0e001f0a58705dae5785e23c3a
tosshii <sj.syj.bumo@gmail.com>
2020-08-17 14:32:34 +0900

    スキルメーターを追加

--- page-about.php
+++ page-about.php
@@ -39,22 +39,8 @@ Template Name: プロフィール
     <section class="section">
       <div class="section-inner">
         <div class="section-content">
-          <div class="skills">
-            <p class="skill-head">スキル</p>
-            <ul class="skill-item">
-              <li>HTML、CSS、SCSS、Bootstrap、</li>
-              <li>JavaScript、jQuery、PHP、</li>
-              <li>WordPress、<a href="https://github.com/totototosshii?tab=repositories/" target="_blank" rel="noopener noreferrer">GitHub</a>、SourceTree、</li>
-              <li>Photoshop、XD、Figma</li>
-            </ul><!-- /.skill-item -->
-            <p class="qualification-head">資格</p>
-            <ul class="qualification-item">
-              <li>Microsoft Office Specialist Excel 365&2019</li>
-              <li>Microsoft Office Specialist Word 2016</li>
-            </ul><!-- /.qualification -->
-          </div><!-- /.skills -->
           <div class="environment">
-            <p class="environment-head">作業環境</p><!-- /.environment-head -->
+            <h4 class="environment-head">作業環境</h4><!-- /.environment-head -->
             <dl class="environment-item">
               <dt class="environment-list">PC：</dt><!-- /.environment-list -->
               <dd class="environment-text">Mac OS</dd><!-- /.environment-text -->
@@ -64,14 +50,193 @@ Template Name: プロフィール
               <dd class="environment-text">Live Sass Compiler</dd><!-- /.environment-text -->
               <dt class="environment-list">FTP：</dt><!-- /.environment-list -->
               <dd class="environment-text">FileZilla</dd><!-- /.environment-text -->
-              <dt class="environment-list">デザイン：</dt><!-- /.environment-list -->
+              <dt class="environment-list">デザイン・書き出し：</dt><!-- /.environment-list -->
               <dd class="environment-text">XD / Figma</dd><!-- /.environment-text -->
-              <dt class="environment-list">画像加工：</dt><!-- /.environment-list -->
+              <dt class="environment-list">画像編集：</dt><!-- /.environment-list -->
               <dd class="environment-text">Photoshop</dd><!-- /.environment-text -->
-              <dt class="environment-list">ローカル環境：</dt><!-- /.environment-list -->
+              <dt class="environment-list">ローカル開発環境：</dt><!-- /.environment-list -->
               <dd class="environment-text">Local by Flywheel</dd>
             </dl><!-- /.environment-item -->
           </div><!-- /.environment -->
+          <div class="qualification">
+            <h4 class="qualification-head">資格</h4>
+            <ul class="qualification-item">
+              <li>Microsoft Office Specialist Excel 365&2019</li>
+              <li>Microsoft Office Specialist Word 2016</li>
+            </ul><!-- /.qualification -->
+          </div><!-- /.qualification -->
+        </div><!-- /.section-content -->
+        <div class="section-content">
+          <div class="skills">
+            <h4 class="skill-head">スキル</h4>
+            <ul class="skill-items">
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-html-5.svg')); ?>" alt="html5のアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  HTML
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-css3.svg')); ?>" alt="CSSのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  CSS
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-sass.svg')); ?>" alt="Sassのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  Sass(SCSS)
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-bootstrap.svg')); ?>" alt="Bootstrapのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  Bootstrap
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-javascript.svg')); ?>" alt="JavaScriptのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  JavaScript
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-jquery.svg')); ?>" alt="jQueryのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  jQuery
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-php.svg')); ?>" alt="PHPのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  PHP
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-wordpress.svg')); ?>" alt="WordPressのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  WordPress
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-github.svg')); ?>" alt="GitHubのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  <a href="https://github.com/totototosshii?tab=repositories/" target="_blank" rel="noopener noreferrer">GitHub</a>
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-sourcetree.svg')); ?>" alt="SourceTreeのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  SourceTree
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-adobe-photoshop.svg')); ?>" alt="Photoshopのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  Photoshop
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-adobe-xd.svg')); ?>" alt="XDのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  XD
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-figma.svg')); ?>" alt="Figmaのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  Figma
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-word.svg')); ?>" alt="Wordのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  Word
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+              <li class="skill-item">
+                <figure class="skill-icon">
+                  <img src="<?php echo esc_url(get_theme_file_uri('images/icons8-excel.svg')); ?>" alt="Excelのアイコン">
+                </figure><!-- /.skill-icon -->
+                <p class="skill-name">
+                  Excel
+                </p><!-- /.skill-name -->
+                <div class="rateoya">
+                  <div class="rate"></div>
+                </div>
+              </li><!-- /.skill-item -->
+            </ul><!-- /.skill-items -->
+          </div><!-- /.skills -->
         </div><!-- /.section-content -->
       </div><!-- /.section-inner -->
     </section><!-- /.section -->
@@ -83,15 +248,15 @@ Template Name: プロフィール
             <p class="career-head">経歴</p><!-- /.career-head -->
             <dl class="career-item">
               <dt class="career-list">2008年</dt><!-- /.career-list -->
-                <dd class="career-text">工業高校卒業後、地元の製造業に就職し工場勤務。<br>主に工作機械のオペレーター業務を担当し、空気圧制御機器の部品を加工。一時期は切削工具管理を兼務し書類作成業務を行う。<br>リーマンショック回復後からは３交代勤務で主力製品の生産に携わる。</dd><!-- /.career-text -->
-                <dt class="career-list">2018年</dt><!-- /.career-list -->
-                <dd class="career-text">体調が優れない事が多くなり、次第に今後の働き方を考えるようになる。<br>調べる過程でWeb制作に興味を持ち、まずはPCスキルを身につけるためにタイピング練習を開始。</dd><!-- /.career-text -->
-                <dt class="career-list">2019年</dt><!-- /.career-list -->
-                <dd class="career-text">交代勤務を続けながら平日は30分〜１時間、休日は３時間の独学。<br>HTML/CSSをProgate、ドットインストールで独学し、初めての言語に難しさを痛感した反面、思い通りの結果が表示させられた時の喜びを実感。<br>学習環境と転職先を考慮し、仙台に拠点を移す事を決意。<br>10月に退職後、プログラミングスクールに通う。</dd><!-- /.career-text -->
-                <dt class="career-list">2020年</dt><!-- /.career-list -->
-                <dd class="career-text">プログラミングスクール以外にも学習の幅を広げる。<br><a href="https://www.jun-akiba.com/category/%e3%82%a4%e3%83%99%e3%83%b3%e3%83%88/" target="_blank" rel="noopener noreferrer">アロキャン</a>への参加、<a href="https://www.daily-trial.com/" target="_blank" rel="noopener noreferrer">デイトラ</a> 、<a href="https://crestadesign.org/" target="_blank" rel="noopener noreferrer">クリ★スタ</a>、<a href="https://ninjacode.work/practice/" target="_blank" rel="noopener noreferrer">忍者CODE</a>などの教材に取り組みながらポートフォリオの作成に取り掛かりました。</dd><!-- /.career-text -->
-                <dt class="career-list">現在</dt><!-- /.career-list -->
-                <dd class="career-text">ポートフォリオ作成と並行して転職活動中。<br><span>一緒に働かせて頂けるWeb制作会社を探しております！！</span></dd><!-- /.career-text -->
+              <dd class="career-text">工業高校卒業後、地元の製造業に就職し工場勤務。<br>主に工作機械のオペレーター業務を担当し、空気圧制御機器の部品を加工。一時期は切削工具管理を兼務し書類作成業務を行う。<br>リーマンショック回復後からは３交代勤務で主力製品の生産に携わる。</dd><!-- /.career-text -->
+              <dt class="career-list">2018年</dt><!-- /.career-list -->
+              <dd class="career-text">体調が優れない事が多くなり、次第に今後の働き方を考えるようになる。<br>調べる過程でWeb制作に興味を持ち、始めにPCスキルを身につけるためにタイピング練習を開始。</dd><!-- /.career-text -->
+              <dt class="career-list">2019年</dt><!-- /.career-list -->
+              <dd class="career-text">交代勤務を続けながら平日は30分〜１時間、休日は３時間の独学。<br>HTML / CSSをProgate、ドットインストールで独学し、初めての言語に難しさを痛感した反面、思い通りの結果が表示させられた時の喜びを実感。<br>学習環境と転職先を考慮し、仙台に拠点を移す事を決意。<br>10月に退職後、プログラミングスクールに通う。</dd><!-- /.career-text -->
+              <dt class="career-list">2020年</dt><!-- /.career-list -->
+              <dd class="career-text">プログラミングスクール以外にも学習の幅を広げる。<br><a href="https://www.jun-akiba.com/category/%e3%82%a4%e3%83%99%e3%83%b3%e3%83%88/" target="_blank" rel="noopener noreferrer">アロキャン</a>への参加、<a href="https://www.daily-trial.com/" target="_blank" rel="noopener noreferrer">デイトラ</a> 、<a href="https://crestadesign.org/" target="_blank" rel="noopener noreferrer">クリ★スタ</a>、<a href="https://ninjacode.work/practice/" target="_blank" rel="noopener noreferrer">忍者CODE</a>などの教材に取り組みながらポートフォリオの作成に取り掛かりました。</dd><!-- /.career-text -->
+              <dt class="career-list">現在</dt><!-- /.career-list -->
+              <dd class="career-text">ポートフォリオ作成と並行して転職活動中。<br><span class="emphasis">一緒に働かせて頂けるWeb制作会社を探しております！！</span><!-- /.emphasis --></dd><!-- /.career-text -->
             </dl><!-- /.career-item -->
           </div><!-- /.career -->
         </div><!-- /.section-content -->
@@ -100,14 +265,12 @@ Template Name: プロフィール
 
     <section class="section">
       <div class="section-inner">
-        <div class="section-content">
-          <div class="future-title">これからの事</div><!-- /.future-title -->
-          <div class="future">
-            <p class="future-message">
-              現在はコーディングがメインですが、デザインのスキルも徐々に伸ばして行き、更に仕事の幅を広げたいと考えております。
-            </p><!-- /.future-message -->
-          </div><!-- /.future-->
-        </div><!-- /.section-content -->
+        <div class="future-title">これからの事</div><!-- /.future-title -->
+        <div class="future">
+          <p class="future-message">
+            現在はコーディングがメインですが、デザインのスキルも徐々に伸ばして行き、更に仕事の幅を広げたいと考えております。
+          </p><!-- /.future-message -->
+        </div><!-- /.future-->
       </div><!-- /.section-inner -->
     </section><!-- /.section -->
 
