2d4e9e0156b1b5074bcad4a47677368f467d8f21
tosshii <sj.syj.bumo@gmail.com>
2020-08-25 15:08:24 +0900

    個別投稿ページにAdvanced Custom Fieldsを導入

--- single.php
+++ single.php
@@ -17,6 +17,55 @@
           <?php the_post(); ?>
           <div class="section-inner">
             <h2 class="section-title"><?php the_title(); ?></h2><!-- /.section-title -->
+            <!-- ACF投稿 -->
+            <?php
+              $img = get_field('repeat_img');
+              $alt= $img['alt'];
+              $size = 'large'; // (thumbnail, medium, large, fullもしくは追加したサイズ)
+              $thumb = $img['sizes'][ $size ];
+              if ($img):
+            ?>
+              <div class="mock-up">
+                <img src="<?php echo $thumb; ?>" alt="<?php echo $alt ?>">
+              </div><!-- /.mock-up -->
+            <?php endif; ?>
+            <dl class="section-explanation">
+              <dt class="explanation-list">経緯</dt><!-- /.explanation-list -->
+              <dd class="explanation-text"><?php the_field('repeat_happened'); ?>
+              </dd><!-- /.explanation-text -->
+              <dt class="explanation-list">担当範囲</dt><!-- /.explanation-list -->
+              <dd class="explanation-text"><?php the_field('repeat_responsibility'); ?></dd><!-- /.explanation-text -->
+              <dt class="explanation-list">ページ数</dt><!-- /.explanation-list -->
+              <dd class="explanation-text"><?php the_field('repeat_pages'); ?></dd><!-- /.explanation-text -->
+              <dt class="explanation-list">レスポンシブ</dt><!-- /.explanation-list -->
+              <dd class="explanation-text"><?php the_field('repeat_responsive'); ?></dd><!-- /.explanation-text -->
+              <dt class="explanation-list">制作期間</dt><!-- /.explanation-list -->
+              <dd class="explanation-text"><?php the_field('repeat_period'); ?></dd><!-- /.explanation-text -->
+              <dt class="explanation-list">使用ツール</dt><!-- /.explanation-list -->
+              <dd class="explanation-text"><?php the_field('repeat_tools'); ?></dd><!-- /.explanation-text -->
+              <dt class="explanation-list">使用言語</dt><!-- /.explanation-list -->
+              <dd class="explanation-text"><?php the_field('repeat_language'); ?></dd><!-- /.explanation-text -->
+              <dt class="explanation-list">ソースコード</dt><!-- /.explanation-list -->
+              <?php
+                $code = get_field('repeat_code');
+                if($code) :
+              ?>
+                <dd class="explanation-text"><a href="<?php the_field('repeat_code'); ?>" target="_blank" rel="noopener noreferrer">GitHub<i class="fab fa-github"></i></a></dd><!-- /.explanation-text --></i>
+              <?php else : ?>
+                <dd class="explanation-text">非公開</dd><!-- /.explanation-text --></i>
+              <?php endif; ?>
+              <dt class="explanation-list">サイト</dt><!-- /.explanation-list -->
+              <?php
+                $site = get_field('repeat_site');
+                if($site) :
+              ?>
+                <dd class="explanation-text"><a href="<?php the_field('repeat_site'); ?>" target="_blank" rel="noopener noreferrer">公開中</a></dd><!-- /.explanation-text -->
+              <?php else : ?>
+                <dd class="explanation-text">非公開</dd><!-- /.explanation-text -->
+              <?php endif; ?>
+            </dl><!-- /.section-explanation -->
+
+            <!-- 通常の投稿 -->
             <?php the_content(); ?>
           </div><!-- /.section-inner -->
         <?php endwhile; ?>
